d0cec84ce0b0af855cbbf1489c08dece681aba77
guido morabito <guidomorabito161@hotmail.com>
2026-05-21 12:23:40 -0300

    backend setup

---
+++ backend/proceso-desarrollo-software/src/main/java/com/pds/proceso_desarrollo_software/ProcesoDesarrolloSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.pds.proceso_desarrollo_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesoDesarrolloSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesoDesarrolloSoftwareApplication.class, args);
+	}
+
+}
---
+++ backend/proceso-desarrollo-software/src/test/java/com/pds/proceso_desarrollo_software/ProcesoDesarrolloSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.proceso_desarrollo_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesoDesarrolloSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
