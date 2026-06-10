d2a03532ad31e19f245354f72b4e0f58455de3d5
guido morabito <guidomorabito161@hotmail.com>
2026-06-09 23:25:44 -0300

    imgs added

--- backend/riva/src/main/java/com/riva/config/DataSeeder.java
+++ backend/riva/src/main/java/com/riva/config/DataSeeder.java
@@ -17,8 +17,13 @@ import com.riva.service.ProductService;
 
 @Component
 public class DataSeeder implements CommandLineRunner {
-
-    private static final String IMAGE_URL = "https://placehold.co/800x1000?text=RIVA";
+    private static final String WOMAN_TSHIRT_IMAGE_URL = "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_t.png";
+    private static final String WOMAN_PANTS_IMAGE_URL = "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_t.png";
+    private static final String WOMAN_JACKET_IMAGE_URL = "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_t.png";
+    private static final String MAN_TSHIRT_IMAGE_URL = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png";
+    private static final String MAN_PANTS_IMAGE_URL = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png";
+    private static final String MAN_JACKET_IMAGE_URL = "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png";
+    private static final String KIDS_TSHIRT_IMAGE_URL = "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png";
 
     private final CategoryRepository categoryRepository;
     private final ProductRepository productRepository;
@@ -53,38 +58,45 @@ public class DataSeeder implements CommandLineRunner {
 
         product("Remera Essential Mujer", "Remera de algodon premium con calce relajado.",
                 "Algodon pima", BigDecimal.valueOf(42), remerasMujer.getId(),
+                WOMAN_TSHIRT_IMAGE_URL,
                 variants(new ProductVariantDto(Size.S, "Blanco", 8), new ProductVariantDto(Size.M, "Negro", 5)));
         product("Pantalon Sastrero Mujer", "Pantalon recto para uso urbano y oficina.",
                 "Viscosa sastrera", BigDecimal.valueOf(96), pantalonesMujer.getId(),
+                WOMAN_PANTS_IMAGE_URL,
                 variants(new ProductVariantDto(Size.M, "Negro", 4), new ProductVariantDto(Size.L, "Gris", 2)));
         product("Campera Liviana Mujer", "Campera corta repelente al agua para media estacion.",
                 "Nylon tecnico", BigDecimal.valueOf(148), camperasMujer.getId(),
+                WOMAN_JACKET_IMAGE_URL,
                 variants(new ProductVariantDto(Size.M, "Arena", 3), new ProductVariantDto(Size.L, "Negro", 0)));
 
         product("Remera Boxy Hombre", "Remera boxy fit de jersey pesado.",
                 "Jersey de algodon", BigDecimal.valueOf(45), remerasHombre.getId(),
+                MAN_TSHIRT_IMAGE_URL,
                 variants(new ProductVariantDto(Size.M, "Crudo", 6), new ProductVariantDto(Size.L, "Negro", 7)));
         product("Pantalon Cargo Hombre", "Cargo urbano con bolsillos laterales y cintura regulable.",
                 "Gabardina", BigDecimal.valueOf(108), pantalonesHombre.getId(),
+                MAN_PANTS_IMAGE_URL,
                 variants(new ProductVariantDto(Size.L, "Verde", 4), new ProductVariantDto(Size.XL, "Negro", 3)));
         product("Campera Utility Hombre", "Campera funcional con multiples bolsillos.",
                 "Ripstop", BigDecimal.valueOf(162), camperasHombre.getId(),
+                MAN_JACKET_IMAGE_URL,
                 variants(new ProductVariantDto(Size.L, "Azul", 2), new ProductVariantDto(Size.XL, "Negro", 1)));
 
         product("Remera Mini RIVA", "Remera infantil suave para uso diario.",
                 "Algodon", BigDecimal.valueOf(34), remerasNinos.getId(),
+                KIDS_TSHIRT_IMAGE_URL,
                 variants(new ProductVariantDto(Size.XS, "Blanco", 5), new ProductVariantDto(Size.S, "Rojo", 4)));
     }
 
     private void product(String name, String description, String material, BigDecimal price, String categoryId,
-                         List<ProductVariantDto> variants) {
+                         String imageUrl, List<ProductVariantDto> variants) {
         productService.create(new CreateProductRequest(
                 name,
                 description,
                 price,
                 material,
                 categoryId,
-                List.of(IMAGE_URL),
+                List.of(imageUrl),
                 variants
         ));
     }
